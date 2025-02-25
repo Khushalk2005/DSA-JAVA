36e916ea7da88363cd19b34ed7aba27824b8003c
Khushalk2005 <khushalkhanna24744@gmail.com>
2025-02-25 22:50:42 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,17 @@
+public class first{
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        int salary = 25400;
+        if (salary > 10000) {
+            salary = salary + 2000 ;
+            
+        }
+        else{
+            salary = salary + 1000;
+        }
+        System.out.println(salary);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
